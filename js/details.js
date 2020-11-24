d3abe8d77ed4fae2992d59682a41dfdc324a2627
Francis Lemaire <lemairefrancis10@gmail.com>
2020-11-24 06:19:14 -0500

    Changed the details display Instead of showing a modal, display details in a fullpage

---
+++ js/details.js
@@ -0,0 +1,247 @@
+var DataStore=null;
+
+fetch("/data/libs.json")
+    .then(response => response.json())
+    .then(data => {
+        DataStore = data;
+        syncStateFromParams();
+    });
+
+function syncStateFromParams(){
+    var url = new URL(window.location.href);
+    var params = url.searchParams;
+    var packageName = params.get("package");
+
+    if (packageName && Object.values(DataStore).map(x => x.name).includes(packageName)) {
+        packageName = decodeURIComponent( packageName);
+        var found = DataStore.filter(x => x.name === packageName);
+        if(found && found.length>0){
+            displayPackageInfos(found[0]);
+        }
+    }
+}
+
+function displayPackageInfos(package){
+
+    var name = document.getElementById("name");
+    name.innerHTML = package.name;
+
+    var version = document.getElementById("version");
+    version.innerHTML = package["version-string"];
+
+    var supports = document.getElementById("supports");
+    // group array
+    var groupedSupport = Object.entries(package["status"])
+        .reduce((result, element) =>{
+            result[element[0].includes("win") || element[0].includes("uwp")?0:element[0].includes("osx")?1:element.includes("linux")?2:3].push(element);
+            return result;
+        },[[],[],[],[]])
+        .reduce((arr,elem) => {return arr.concat(elem);},[]);
+    for( let item of groupedSupport){
+        if (item[1] === "pass") {
+            let span = document.createElement("span");
+            span.className = "tag is-primary has-text-weight-bold";
+            span.innerText += item[0];
+
+            if(item[0].includes("win") || item[0].includes("uwp")){
+                let ispan = document.createElement("span");
+                ispan.className = "icon";
+                let icon = document.createElement("i");
+                icon.className = "fab fa-windows"
+                ispan.appendChild(icon);
+                span.insertAdjacentElement('afterbegin',ispan);
+
+            } else if(item[0].includes("osx")){
+                let ispan = document.createElement("span");
+                ispan.className = "icon";
+                let icon = document.createElement("i");
+                icon.className = "fab fa-apple"
+                ispan.appendChild(icon);
+                span.insertAdjacentElement('afterbegin',ispan);
+            } else if(item[0].includes("linux")){
+                let ispan = document.createElement("span");
+                ispan.className = "icon";
+                let icon = document.createElement("i");
+                icon.className = "fab fa-linux"
+                ispan.appendChild(icon);
+                span.insertAdjacentElement('afterbegin',ispan);
+            }
+
+            supports.appendChild(span);
+        }
+    }
+    
+
+
+    var description = document.getElementById("description");
+    description.innerHTML = package.description;
+
+    var installwin = document.getElementById("installwin");
+    installwin.innerText = `vcpkg.exe install ${package.name}`;
+
+    var installunix = document.getElementById("installunix");
+    installunix.innerText = `vcpkg install ${package.name}`;
+
+    var installwinbtn = document.getElementById("installwinbtn");
+    installwinbtn.addEventListener("click", function(){ copyTextToClipboard(`vcpkg.exe install ${package.name}`)},false);
+
+    var installunixbtn = document.getElementById("installunixbtn");
+    installunixbtn.addEventListener("click", function(){ copyTextToClipboard(`vcpkg install ${package.name}`)},false);
+
+
+    var installwinslct = document.getElementById("installwinslct");
+    var installunixslct = document.getElementById("installunixslct");
+    if (navigator.appVersion.indexOf("Win")!=-1){
+        installwinslct.checked = true;
+    }else{
+        installunixslct.checked = true;
+    }
+
+    var usage = document.getElementById("usage");
+    if(package.usage){
+        usage.innerHTML = package.usage;
+        usage.parentNode.classList.remove("is-hidden");
+    }else{
+        usage.innerHTML = "";
+        if(!usage.parentNode.classList.contains("is-hidden")){
+            usage.parentNode.classList.add("is-hidden");
+        }
+    }
+
+    var features = document.getElementById("features");
+    features.textContent ='';
+    if("features" in package){
+        for( let item of Object.values(package["features"])){
+            let tr = document.createElement("tr");
+            let thname = document.createElement("th");
+            thname.innerText = item.name;
+            tr.appendChild(thname);
+            let description = document.createElement("td");
+            description.innerHTML = item.description;
+            tr.appendChild(description);
+            
+            features.appendChild(tr);
+        }
+    }
+
+    var builddepends = document.getElementById("builddepends");
+    builddepends.textContent = '';
+    if("dependencies" in package){
+        for(let item of package["dependencies"].map(x => (typeof x == 'string'? x: x.name) )){
+            var tag = document.createElement("a");
+            tag.innerText = item;
+            tag.className = "tag is-link";
+            if(Object.values(DataStore).map(x => x.name).includes(item.match(/[\w-]+/).toString()))
+            {
+                tag.href = `/details.html?package=${item.match(/[\w-]+/)}`;
+            }
+            builddepends.appendChild(tag);
+        }
+    }
+
+    var license = document.getElementById("license");
+    var licenseText = package.license;
+    if (licenseText == null) {
+        licenseText = "Unspecified";
+    }
+    license.insertAdjacentText('beforeend', " "+licenseText);
+
+    var homepage = document.getElementById("homepage");
+    var homepageURL = package.homepage;
+    if (homepageURL == null) {
+        homepageURL = "#";
+    }
+    homepage.href = homepageURL;
+
+    // Github only has 60 request/hour for unauth requests
+    // var match = homepageURL.match(/github.com((?:\/[-\w]*){2})/)
+    // if(match.length >= 2){
+    //     match[1]
+    //     fetch(`https://api.github.com/repos${match[1]}`)
+    //     .then(function(response) {
+    //         if (!response.ok) {
+    //             response.
+    //             throw Error(response.statusText);
+    //         }
+    //         return response;
+    //     })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         addStats(data);
+    //         syncStateFromParams();
+    //     }).catch(function(error) {
+    //         console.log(error);
+    //     });
+    // }
+
+    var documentation = document.getElementById("documentation");
+    var documentationURL = package.documentation;
+    if (documentationURL == null) {
+        documentationURL = "#";
+    }
+    documentation.href = documentationURL;
+
+    var maintainers = document.getElementById("maintainers");
+    for (const nameemail of package.maintainers) {
+        match = nameemail.match(/(.+)(?=<.*>)/);
+        if(match.length>=1){
+            let name = document.createElement("p");
+            name.className = "subtitle is-5"
+            name.innerText = match[1];
+            maintainers.insertAdjacentElement('beforeend', name);
+        }
+    }
+
+   
+
+}
+
+function addStats(data){
+    var infos = document.getElementById("infos");
+    infos.insertAdjacentText('afterend', data);
+
+}
+
+function collapse(){
+    var elem = event.target.parentNode;
+    if(elem.classList.contains("collapsable")){
+        elem.classList.toggle("collapsed");
+
+    }
+}
+
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+    //   console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+    //   console.error('Fallback: Oops, unable to copy', err);
+    }
+  
+    document.body.removeChild(textArea);
+  }
+
+  function copyTextToClipboard(text) {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text);
+      return;
+    }
+    navigator.clipboard.writeText(text).then(function() {
+    //   console.log('Async: Copying to clipboard was successful!');
+    }, function(err) {
+    //   console.error('Async: Could not copy text: ', err);
+    });
+  }
--- js/search.js
+++ js/search.js
@@ -47,8 +47,9 @@ function syncStateFromParams(){
         renderResult();
     }
 
+    // kept for backward compatibility with first version of the site
     if(modal && Object.values(DataStore).map(x => x.name).includes(modal)){
-        displayModal(DataStore.filter(x => x.name === modal)[0]);
+        navigateToDetails(modal);
     }
 }
 
@@ -85,7 +86,7 @@ function updateURLParams(){
 
     url.search = params.toString();
 
-    window.history.replaceState({}, searchbox.value, url.toString());
+    window.history.replaceState({}, searchbox.value, url.search);
 }
 
 
@@ -141,7 +142,7 @@ function renderRow(itemName){
     var output = document.getElementById("searchResults");
     var entry = document.createElement("tr");
     entry.className = "container is-clickable";
-    entry.addEventListener("click",function(){displayModal(DataItem);}, false);
+    entry.addEventListener("click",function(){navigateToDetails(itemName)}, false);
     var name = document.createElement("th");
     name.innerHTML = itemName;
     entry.appendChild(name);
@@ -176,126 +177,8 @@ function renderRow(itemName){
     output.appendChild(entry)
 }
 
-function displayModal(data){
-    currmodal = data.name;
-    updateURLParams();
-
-    var modalbase = document.getElementById("modalbase");
-    modalbase.className = "modal is-active";
-
-    var modalname = document.getElementById("modalname");
-    modalname.innerHTML = data.name;
-
-    var modalversion = document.getElementById("modalversion");
-    modalversion.innerHTML = data["version-string"];
-
-    var modalwebsite = document.getElementById("modalwebsite");
-    if(data.homepage){
-        modalwebsite.href = data.homepage;
-        modalwebsite.classList.remove("is-invisible");
-    }else{
-        modalwebsite.href = ""
-        if(!modalwebsite.classList.contains("is-invisible")){
-            modalwebsite.classList.add("is-invisible");
-        }
-
-    }
-
-    var modallicense = document.getElementById("modallicense");
-    var modallicensetxt = document.getElementById("modallicensetxt");
-    if(data.license){
-        modallicense.innerHTML = data.license;
-        modallicense.classList.remove("is-hidden");
-        modallicensetxt.classList.remove("is-hidden");
-    }else{
-        modallicense.innerHTML = ""
-        if(!modallicense.classList.contains("is-hidden")){
-            modallicense.classList.add("is-hidden");
-        }
-        if(!modallicensetxt.classList.contains("is-hidden")){
-            modallicensetxt.classList.add("is-hidden");
-        }
-
-    }
-
-    var modaldescription = document.getElementById("modaldescription");
-    modaldescription.innerHTML = data.description;
-
-    var modalwin = document.getElementById("modalwin");
-    modalwin.innerText = `vcpkg.exe install ${data.name}`;
-
-    var modalunix = document.getElementById("modalunix");
-    modalunix.innerText = `vcpkg install ${data.name}`;
-
-    var modalwinbtn = document.getElementById("modalwinbtn");
-    modalwinbtn.replaceWith(modalwinbtn.cloneNode(true)); // Remove all event listeners
-    modalwinbtn = document.getElementById("modalwinbtn");
-    modalwinbtn.addEventListener("click", function(){ copyTextToClipboard(`vcpkg.exe install ${data.name}`)},false);
-
-    var modalwinslct = document.getElementById("modalwinslct");
-    var modalunixslct = document.getElementById("modalunixslct");
-    if (navigator.appVersion.indexOf("Win")!=-1){
-        modalwinslct.checked = true;
-    }else{
-        modalunixslct.checked = true;
-    }
-
-
-    var modalunixbtn = document.getElementById("modalunixbtn");
-    modalunixbtn.replaceWith(modalunixbtn.cloneNode(true)); // Remove all event listeners
-    modalunixbtn = document.getElementById("modalunixbtn");
-    modalunixbtn.addEventListener("click", function(){ copyTextToClipboard(`vcpkg install ${data.name}`)},false);
-
-
-    var modalusage = document.getElementById("modalusage");
-    if(data.usage){
-        modalusage.innerHTML = data.usage;
-        modalusage.parentNode.classList.remove("is-hidden");
-    }else{
-        modalusage.innerHTML = "";
-        if(!modalusage.parentNode.classList.contains("is-hidden")){
-            modalusage.parentNode.classList.add("is-hidden");
-        }
-    }
-
-    var modalfeatures = document.getElementById("modalfeatures");
-    modalfeatures.textContent ='';
-    if("features" in data){
-        for( let item of Object.values(data["features"])){
-            let tr = document.createElement("tr");
-            let thname = document.createElement("th");
-            thname.innerText = item.name;
-            tr.appendChild(thname);
-            let description = document.createElement("td");
-            description.innerHTML = item.description;
-            tr.appendChild(description);
-            
-            modalfeatures.appendChild(tr);
-        }
-    }
-
-    var modalbdepends = document.getElementById("modalbdepends");
-    modalbdepends.textContent = '';
-    if("dependencies" in data){
-        for(let item of data["dependencies"].map(x => (typeof x == 'string'? x: x.name) )){
-            var tag = document.createElement("a");
-            tag.innerText = item;
-            tag.className = "tag is-link";
-            if(Object.values(DataStore).map(x => x.name).includes(item.match(/[\w-]+/).toString()))
-            {
-                tag.href = `/?modal=${item.match(/[\w-]+/)}`;
-            }
-            modalbdepends.appendChild(tag);
-        }
-    }
-
-}
-
-function hiddeModal(){
-    var modalbase = document.getElementById("modalbase");
-    modalbase.className = "modal"
-    currmodal = "";
-    updateURLParams();
+function navigateToDetails(packageName){
+    window.location = `/details.html?package=${encodeURIComponent(packageName)}`;
 }
 
 function nextpage(){
@@ -342,39 +225,3 @@ function collapse(){
 
     }
 }
-
-function fallbackCopyTextToClipboard(text) {
-    var textArea = document.createElement("textarea");
-    textArea.value = text;
-    
-    // Avoid scrolling to bottom
-    textArea.style.top = "0";
-    textArea.style.left = "0";
-    textArea.style.position = "fixed";
-  
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-  
-    try {
-      var successful = document.execCommand('copy');
-      var msg = successful ? 'successful' : 'unsuccessful';
-    //   console.log('Fallback: Copying text command was ' + msg);
-    } catch (err) {
-    //   console.error('Fallback: Oops, unable to copy', err);
-    }
-  
-    document.body.removeChild(textArea);
-  }
-
-  function copyTextToClipboard(text) {
-    if (!navigator.clipboard) {
-      fallbackCopyTextToClipboard(text);
-      return;
-    }
-    navigator.clipboard.writeText(text).then(function() {
-    //   console.log('Async: Copying to clipboard was successful!');
-    }, function(err) {
-    //   console.error('Async: Could not copy text: ', err);
-    });
-  }
