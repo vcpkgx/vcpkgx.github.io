23653582f0c4b8e8f9f76677dd4f40edac9662b1
Francis Lemaire <lemairefrancis10@gmail.com>
2022-11-29 00:10:22 -0500

    Tentative fix for build

--- .tools/parsers/status_parser.py
+++ .tools/parsers/status_parser.py
@@ -3,14 +3,14 @@ def evalEBNF(ebnf:str, platform:dict)->bool:
     try:
         return eval(ebnf,platform)
     except NameError as e:
-        print("NE:", e.name)
+        print("NE:", e)
         return True
 
 
 triplet_patch = ["native", "staticcrt"]
 def allTargetsDict(triplets:dict, defaultValue:bool)->dict:
     out= {} 
-    for group in triplets.values():
+    for group in [triplets["built-in"]]:
         for triplet in group: 
             out.update(dict((x,defaultValue) for x in triplet.split("-")))
 
